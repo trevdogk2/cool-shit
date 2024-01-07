41f13f9a7c1ec3398e85c193129671ff1f8a65b7
Trevor Westin <trevorwwestin@gmail.com>
2024-01-07 16:06:35 -0500

    next 14.0.1 downgrade for webgl bug temp fix

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         <ConsoleArt />
-        <PageCrack toggle={true}>
+        <PageCrack toggle={false}>
           <Header />
           {children}
         </PageCrack>
